80accf88211d21ae2ba84702f99966bd75e790ed
ailyahjo <jordan.aliy@northeastern.edu>
2024-01-18 09:37:52 -0500

    Created connected to server function, and parse command line function

--- Project 1/client.py
+++ Project 1/client.py
@@ -13,15 +13,44 @@ This client program support TLS encrypted sockets
 # Import argsparse for dealing with command prompt
 import argparse
 
-# create a parser object
-parser = argparse.ArgumentParser(description='Command Parser for Client Program')
+'''
+This function parses the command line arguments. 
+'''
+def parse_args() :
 
-# add arguments to the parser object
-parser.add_argument('-p', type=int, help= "Port Number", required=False)
-args = parser.parse_args()
-print(args)
-parser.add_argument('-s', type=str, help= "Server Name", required=False)
-parser.add_argument(type=str, help= "Host Name", required = True)
-parser.add_argument(type=str, help = "Northeastern Username", required = True)
+    # create a parser object
+    parser = argparse.ArgumentParser(description='Command Parser for Client Program')
+
+    # add arguments to the parser object
+    parser.add_argument('-p', type=int, help= "Port Number", required=False)
+    parser.add_argument('-s', type=str, help= "Server Name", required=False)
+    parser.add_argument('HostName', type=str, help= "Host Name" )
+    parser.add_argument('NortheasternUsername', type=str, help= "Northeastern Username")
+
+    return parser.parse_args()
 
 
+'''
+This function is able to connect to the server with parsed argument from the command line.
+Once connected, will send hello message server to initiate protocol for the game.
+'''
+def connect_to_server(HostName, p):
+    # Create a stream socket object
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    socket.connect((HostName, p))
+
+    # Send hello message to server
+    hello_message = '{"type": "hello", "northeastern_username": "<your-My.Northeastern-username>"}\n'
+    socket.sendall(hello_message.encode())    
+
+    
+def main():
+    # Parse the command line arguments
+    args = parse_args()
+
+    # Connect to the server
+    connect_to_server(HostName=args.HostName, p=args.p)
+
+if __name__ == "__main__":
+    main()
