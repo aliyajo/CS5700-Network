1757a16894322f8c41aba4b7fdc8ba10f0dc4d32
ailyahjo <jordan.aliy@northeastern.edu>
2024-01-18 10:26:40 -0500

    Added secret flags  and wordlist file -- working on incorporating the wordlist file in the guess

--- Project 1/client.py
+++ Project 1/client.py
@@ -12,12 +12,14 @@ This client program support TLS encrypted sockets
 '''
 # Import argsparse for dealing with command prompt
 import argparse
+import json
+import socket 
+import json
 
 '''
 This function parses the command line arguments. 
 '''
 def parse_args() :
-
     # create a parser object
     parser = argparse.ArgumentParser(description='Command Parser for Client Program')
 
@@ -34,23 +36,50 @@ def parse_args() :
 This function is able to connect to the server with parsed argument from the command line.
 Once connected, will send hello message server to initiate protocol for the game.
 '''
-def connect_to_server(HostName, p):
-    # Create a stream socket object
-    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    socket.connect((HostName, p))
+def connect_to_server(c_socket, HostName, p, NortheasternUsername):
+    c_socket.connect((HostName, p))
 
     # Send hello message to server
-    hello_message = '{"type": "hello", "northeastern_username": "<your-My.Northeastern-username>"}\n'
-    socket.sendall(hello_message.encode())    
+    hello_message = json.dumps({
+        "type": "hello",
+        "northeastern_username": NortheasternUsername
+    }) + "\n"
 
-    
+    c_socket.send(hello_message.encode())
+
+def guess():
+
+def send_guess(c_socket, guess):
+    # Send guess message to server
+    guess_message = json.dumps({
+        "type": "guess",
+        "guess": guess
+    }) + "\n"
+
+    c_socket.send(guess_message.encode())
+
+def read_response(c_socket):
+    # Read response from server
+    response = c_socket.recv(4096).decode()
+
+    # Parse response from server
+    response = json.loads(response)
+
+    return response
+
 def main():
     # Parse the command line arguments
     args = parse_args()
 
-    # Connect to the server
-    connect_to_server(HostName=args.HostName, p=args.p)
+    # Create a stream socket object
+    c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    # Connect to the server
+    connect_to_server(c_socket, HostName=args.HostName, p=args.p, NortheasternUsername=args.NortheasternUsername)
+    print(read_response(c_socket)) 
+    # Send guess to server
+    guess = guess()
+    send_guess(c_socket, guess)
+    print(read_response(c_socket))
 if __name__ == "__main__":
     main()
