92bbb18888c631fa9bf2fcca4d5021e079ddf9ac
linda <linnppa@gmail.com>
2024-04-15 11:10:38 -0400

    LQ: update scp for geo folder

--- Project 6/scpKhoury.py
+++ Project 6/scpKhoury.py
@@ -2,7 +2,12 @@ import os
 import subprocess
 
 file_list = [
-    "deployCDN", "runCDN", "stopCDN", "dnsserver", "httpserver", "Makefile", "scpBuildServer.py", "README.md"
+    "deployCDN", "runCDN", "stopCDN", "dnsserver", "httpserver", "Makefile", 
+    "scpBuildServer.py", "README.md"
+]
+
+directory_list = [
+    "GeoIp2"
 ]
 
 khoury_address = "linppa@login.khoury.northeastern.edu:~/"
@@ -11,11 +16,16 @@ keyfile = "keys/ssh-ed25519-quach.l.priv"
 
 def scpKhoury():
     for file in file_list:
-        command = f'scp {file} {khoury_address}'
+        command = ['scp', '-i', keyfile, file, f'{khoury_address}']
         print(f"Copying file {file} to Khoury server")
-        subprocess.run(command, shell=True)
+        subprocess.run(command)
         print(f"File {file} has been copied to Khoury server")
+    
+    for directory in directory_list:
+        command = ['scp', '-i', keyfile, '-r', directory, f'{khoury_address}']
+        print(f"Copying directory {directory} to Khoury server")
+        subprocess.run(command)
+        print(f"Directory {directory} has been copied to Khoury server")
 
 if __name__ == "__main__":
     scpKhoury()
-
