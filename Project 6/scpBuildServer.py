d62980e8fa869dc538f8d0f12cacc216f9854790
linda <linnppa@gmail.com>
2024-04-15 23:41:50 -0400

    LQ: update deploy, scp's

--- Project 6/scpBuildServer.py
+++ Project 6/scpBuildServer.py
@@ -6,10 +6,6 @@ file_list = [
     "README.md", "ssh-ed25519-quach.l.priv", "ssh-ed25519-quach.l.pub"
 ]
 
-directory_list = [
-    "GeoIp2"
-]
-
 build_server_address = "linppa@cs5700cdnproject.ccs.neu.edu:~/"
 
 keyfile = "ssh-ed25519-quach.l.priv"
@@ -20,12 +16,6 @@ def scpBuildServer():
         print(f"Copying file {file} to Khoury server")
         subprocess.run(command)
         print(f"File {file} has been copied to Khoury server")
-    
-    for directory in directory_list:
-        command = ['scp', '-i', keyfile, '-r', directory, f'{build_server_address}']
-        print(f"Copying directory {directory} to Khoury server")
-        subprocess.run(command)
-        print(f"Directory {directory} has been copied to Khoury server")
 
 if __name__ == "__main__":
     scpBuildServer()
--- Project 6/scpKhoury.py
+++ Project 6/scpKhoury.py
@@ -6,10 +6,6 @@ file_list = [
     "scpBuildServer.py", "README.md"
 ]
 
-directory_list = [
-    "GeoIp2"
-]
-
 khoury_address = "linppa@login.khoury.northeastern.edu:~/"
 
 keyfile = "keys/ssh-ed25519-quach.l.priv"
@@ -20,12 +16,6 @@ def scpKhoury():
         print(f"Copying file {file} to Khoury server")
         subprocess.run(command)
         print(f"File {file} has been copied to Khoury server")
-    
-    for directory in directory_list:
-        command = ['scp', '-i', keyfile, '-r', directory, f'{khoury_address}']
-        print(f"Copying directory {directory} to Khoury server")
-        subprocess.run(command)
-        print(f"Directory {directory} has been copied to Khoury server")
 
 if __name__ == "__main__":
     scpKhoury()
